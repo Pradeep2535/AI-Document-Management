2a2c47daa657dfb8ea36f4356786ec1fdaf936da
Pradeep S <117376703+Pradeep2535@users.noreply.github.com>
2025-03-07 23:03:20 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,7 @@
+MONGODB_URI = "mongodb+srv://pradeepeng2535:luXt72JOHhh19Lne@cluster0.67vdv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+GOOGLE_API_KEY = 'AIzaSyDY6faZaxhHoc1bobAwOogirwDGbeKkJFA'
+CLIENT_ID = '956792027823-lsvg7hp77324b3kev6an8lbrr4odh8ju.apps.googleusercontent.com'
+CLIENT_SECRET = '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'
+
+REFRESH_TOKEN = '1//04ttYgA-DWqQYCgYIARAAGAQSNwF-L9IrkLEmy_1ywjaHTV--CupdbKuXndRQAB1zkUZ2GrmxbdTkAJ6OWUnTw5hyPNlrrbp2R_A'
+
